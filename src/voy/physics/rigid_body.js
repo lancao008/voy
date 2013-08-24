cd5a8fc390d7ed098be42af7cb9980be5a9d20a4
Rasmus Rønn Nielsen <rasmusrnielsen@gmail.com>
2013-08-24 12:10:15 +0200

    You can now configure RigidBody through its constructor.

--- src/voy/physics/rigid_body.js
+++ src/voy/physics/rigid_body.js
@@ -1,12 +1,13 @@
 Voy.RigidBody = function(options) {
   Voy.Component.call(this, 'rigidBody');
-  this.static = options && options.static || false;
+  options = options || {};
+  this.static = options.static || false;
   this.force = Voy.Vector2.zero();
   this.velocity = Voy.Vector2.zero();
-  this.drag = 0.5;
-  this.maxSpeed = 0.2;
-  this.maxForce = 4;
-  this.bounciness = 0.1;
+  this.drag = options.drag || 0.05;
+  this.maxSpeed = options.maxSpeed || 0.2;
+  this.maxForce = options.maxForce || 0.01;
+  this.bounciness = options.bounciness || 0.1;
 };
 
 Voy.RigidBody.prototype = Object.create(Voy.Component.prototype);
