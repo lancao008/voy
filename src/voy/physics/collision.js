1cbf81169c55df0d6eeb537fdeb869c57dac98f5
Rasmus Rønn Nielsen <rasmusrnielsen@gmail.com>
2013-08-24 14:00:31 +0200

    Collision resolver now calculates correct velocity direction (does not yet take mass into account).

--- src/voy/physics/collision.js
+++ src/voy/physics/collision.js
@@ -16,27 +16,17 @@ Voy.Collision.prototype.notify = function() {
 Voy.Collision.prototype.resolve = function() {
   this[0].localPosition.add(Voy.Vector2.multiply(this.separation, 1.001));
 
-  // we shut probably do something more clever about the velocity
-  this[0].rigidBody.velocity = Voy.Vector2.zero();
-  /*
-  var centerDifference = Voy.Vector2.subtract(this[0].getWorldPosition(), this[1].getWorldPosition());
-  var axis = Math.abs(centerDifference[0]) > Math.abs(centerDifference[1]) ? 0 : 1;
-  var force = this[0].collider.size[axis]/2 + this[0].collider.size[axis]/2 - Math.abs(centerDifference[axis]);
-  if(!this[0].rigidBody.static && !this[1].rigidBody.static) force *= 0.5;
-  var separation = Math.abs(centerDifference[axis])/centerDifference[axis];
-  var translation = Voy.Vector2.zero();
-  translation[axis] = force*separation;
+  var normal = Voy.Vector2.normalize(this.separation);
+
+  var velocityDifference = Voy.Vector2.subtract(this[0].rigidBody.velocity, this[1].rigidBody.velocity);
+
+  var velocityAlongNormal = velocityDifference.getDotProduct(normal);
+
+  if(velocityAlongNormal > 0) return false;
 
   var bounciness = Math.min(this[0].rigidBody.bounciness, this[1].rigidBody.bounciness);
 
-  if(!this[0].rigidBody.static) {
-    this[0].position.add(translation);
-    this[0].rigidBody.velocity[axis] = separation*bounciness;
-  }
-
-  if(!this[1].rigidBody.static) {
-    this[1].position.subtract(translation);
-    this[1].rigidBody.velocity[axis] = separation*-1*bounciness;
-  }
-  */
+  var impulsePower = -velocityAlongNormal*(1+bounciness)*1.01;
+  var impulse = Voy.Vector2.multiply(normal, impulsePower);
+  this[0].rigidBody.velocity.add(impulse);
 };
